4ecaf1c12037878681e0cad265868c8247d07a33
faladetimilehin <faladetimilehin@yahoo.com>
2022-01-12 16:51:08 +0100

    webpack and babel template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+    return (
+        <>
+            Hello World
+        </>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
